e101556b1f399bfd43fc04a63d1e593654948694
naratamaamartaragam <naratamaamartaragam@gmail.com>
2026-08-12 18:09:36 +0700

    Update Vite configuration to remove Laravel plugin
    Removed Laravel Vite plugin configuration and added build output directory.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  // ... konfigurasi lainnya
+  build: {
+    outDir: 'dist',
+  },
+})
